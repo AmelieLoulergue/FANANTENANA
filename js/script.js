c11f168e3c61c1218928282c0cfb27659bd92f9e
Clément L <32853290+LoulergueC@users.noreply.github.com>
2021-04-20 00:02:43 +0200

    Donation section

--- js/script.js
+++ js/script.js
@@ -33,4 +33,19 @@ $('nav').hover(function() {
         'width': '80px',
         'background': 'rgba(0,0,0,.2)'
     });
+});
+
+$('document').ready(function() {
+    var n = 0;
+    setInterval(() => {
+        if (n === 1) {
+            $("#donation").css('background-image', 'url(img/donation1-d.jpg)');
+            $("#donation-light-bg").css('background-image', 'url(img/donation1-l.jpg)');
+            n = 0;
+        } else {
+            $("#donation").css('background-image', 'url(img/donation2-d.jpg)');
+            $("#donation-light-bg").css('background-image', 'url(img/donation2-l.jpg)');
+            n = 1;
+        }
+    }, 5000);
 })
