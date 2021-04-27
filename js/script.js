e1912b0291bd7fa1997f01004b56f63d5ef686e6
Clément L <32853290+LoulergueC@users.noreply.github.com>
2021-04-28 00:58:57 +0200

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -50,6 +50,14 @@ $('document').ready(function() {
             n = 1;
         }
     }, 5000);
+
+
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 1000; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
 });
 
 const swiper = new Swiper('.swiper-container', {
